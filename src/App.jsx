bc47217a62508194b40938c588980c5c89a95419
Pempi <asdd85738@gmail.com>
2026-01-12 20:31:05 -0600

    [fix] .env modify

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import logo from "./assets/movie.svg"
 
-const key = import.meta.env.VITE_OMDB_KEY || "e5d9474c";
+const key = import.meta.env.VITE_TOKEN;
 
 const fetchMovies = async (search) => {
   if (!search) return null;
@@ -62,7 +62,7 @@ export default function App() {
           <input
             type="text"
             className="form-control form-control-lg shadow-sm"
-            style={{width: '400px'}}
+            style={{width: '400px', marginLeft: '110px'}}
             placeholder="Escribe una película (mínimo 3 letras)..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
@@ -71,7 +71,7 @@ export default function App() {
         <div className="col-md-6">
           <button
             className="btn btn-primary btn-lg shadow-sm"
-            style={{marginLeft: '110px'}}
+            style={{marginLeft: '140px'}}
             onClick={() => setSearchTerm("")}
           >
             Limpiar
@@ -90,7 +90,7 @@ export default function App() {
       )}
 
       {isError && (
-        <div className="alert alert-danger d-flex align-items-center animate-shake bg-dark" role="alert">
+        <div className="alert alert-danger d-flex align-items-center animate-shake bg-white text-dark" role="alert">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-exclamation-triangle-fill me-2" viewBox="0 0 16 16">
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
           </svg>
@@ -101,7 +101,7 @@ export default function App() {
       <div className="container-fluid min-h-screen bg-dark py-5">
         
         <div className="row g-4 px-4">
-          {searchTerm.length >= 3 && searchTerm != "" ? (data?.Search?.map((m) => (
+          {searchTerm.length >= 2 && searchTerm != "" ? (data?.Search?.map((m) => (
             <div key={m.imdbID} className="col-6 col-md-4 col-lg-3 animate-fade-in">
               <div className="card h-100 shadow border-0 movie-card bg-secondary text-white">
                 <img
@@ -153,16 +153,19 @@ export default function App() {
               ) : (
                 <>
                   <div className="modal-header border-secondary">
-                    <h5 className="modal-title">{movieDetails?.Title}</h5>
+                    <h5 className="modal-title fw-bold text-warning">{movieDetails?.Title}</h5>
                     <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal"></button>
                   </div>
                   <div className="modal-body">
                     <div className="row">
                       <div className="col-md-4">
-                        <img src={movieDetails?.Poster} className="img-fluid rounded" alt="" />
+                        <img src={movieDetails?.Poster} className="img-fluid rounded shadow-sm hover:`scale(1.05)`" alt="" />
                       </div>
                       <div className="col-md-8">
                         <p className="text-warning fw-bold">⭐ {movieDetails?.imdbRating}</p>
+                        <p className="text-success fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#46e416ff" viewBox="0 0 16 16">
+  <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11V10.937c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.51 5a2 2 0 1 0 .01 4 2 2 0 0 0-.01-4zm10 0a2 2 0 1 0 .01 4 2 2 0 0 0-.01-4z"/>
+</svg> {movieDetails?.Awards}</p>
                         <p><strong>Sinopsis:</strong> {movieDetails?.Plot}</p>
                         <p><strong>Director:</strong> {movieDetails?.Director}</p>
                         <p><strong>Actores:</strong> {movieDetails?.Actors}</p>
@@ -174,13 +177,14 @@ export default function App() {
                         <div className="mt-3">
                         </div>
                         <div className="mt-3">
-                          <span className="badge bg-info text-dark me-2">{movieDetails?.Genre}</span>
-                          <span className="badge bg-info text-dark me-2">{movieDetails?.Runtime}</span>
+                          <span className="badge bg-info text-dark me-2">Genero: {movieDetails?.Genre}</span>
+                          <span className="badge bg-info text-dark me-2">Duracion: {movieDetails?.Runtime}</span>
                         </div>
                         <div className="mt-3">
-                          <span className="badge bg-info text-dark me-2">{movieDetails?.Released}</span>
-                          <span className="badge bg-info text-dark me-2">{movieDetails?.Type}</span>
-                          <span className="badge bg-info text-dark me-2">{movieDetails?.Country}</span>
+                          <span className="badge bg-info text-dark me-2">Lanzamiento: {movieDetails?.Released}</span>
+                          <span className="badge bg-info text-dark me-2">Tipo: {movieDetails?.Type}</span>
+                          <span className="badge bg-info text-dark me-2">Pais: {movieDetails?.Country}</span>
+                          <span className="badge bg-info text-dark me-2">Clasificacion: {movieDetails?.Rated}</span>
                         </div>
                       </div>
                     </div>
